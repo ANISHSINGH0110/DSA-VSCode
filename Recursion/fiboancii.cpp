a58162f9271b14253a77a30c2cd3303d465e62f9
ANISHSINGH0110 <anish2003s@gmail.com>
2025-02-20 09:28:02 +0530

    Power of 2

---
+++ Recursion/fiboancii.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number : " << endl;
+    cin >> n;
+    int p = pow(n);
+    cout << "Power of NUmber is : " << p << endl;
+    return 0;
+
+}
