039f25ab0e3f8ea8f81d93ef3f19969af55430df
ANISHSINGH0110 <anish2003s@gmail.com>
2025-02-24 12:09:18 +0530

    Longest Increasing Subsequence

--- DP/Longest_Increasing_Subsequence.cpp
+++ DP/Longest_Increasing_Subsequence.cpp
@@ -21,7 +21,61 @@ void printAllIncreasingSubsequences(vector<int>& nums) {
     vector<int> temp;  // Temporary vector to store subsequence
     findSubsequences(nums, temp, 0);
 }
+int solve(vector<int>&nums,int last,int i)
+{
+    if(i>=nums.size())
+      return 0;
+    int curr = nums[i];
+    int incl =0;
+    if(curr > last)
+      incl = 1+solve(nums,nums[i],i+1);
+    int excl = 0+solve(nums,last,i+1);
+    return max(incl,excl);    
+}
+int solveMem(vector<int>&nums,int lastI,int i,vector<vector<int>>&dp)
+{
+    if(i>=nums.size())
+      return 0;
+    if(dp[lastI+1][i] != -1)
+      return dp[lastI+1][i];  
+    int curr = nums[i];
+    int incl =0;
+    if(lastI == -1 || curr > nums[lastI])
+      incl = 1+solveMem(nums,i,i+1,dp);
+    int excl = 0+solveMem(nums,lastI,i+1,dp);
+    dp[lastI+1][i] = max(incl,excl);
+    return dp[lastI+1][i];    
+}
+int solveTab(vector<int>&nums)
+{
+    int n = nums.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
 
+    for(int curr = n-1;curr>=0;curr--)
+    {
+        for(int prev = curr-1;prev>= -1;prev--)
+        {
+            int incl = 0;
+            if(prev == -1 || nums[curr] >nums[prev])
+            {
+                incl = 1+dp[curr+1][curr+1];
+            }
+            int excl = 0+dp[curr+1][prev+1];
+            dp[curr][prev+1] = max(incl,excl);
+
+        }
+    }
+   return dp[0][0];
+}
+int lengthOfLIS(vector<int>& nums) {
+    //int last = INT_MIN;
+//    return solve(nums,last,0); 
+    
+    //  vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+1,-1));
+     //return solveMem(nums,-1,0,dp);
+
+     return solveTab(nums);
+}
 int main()
 {
     int n;
@@ -33,8 +87,8 @@ int main()
     {
         cin >> nums[i];
     }
-    //cout << "Ans is " << lengthOfLIS(nums) << endl;
-    printAllIncreasingSubsequences(nums);
+    cout << "Ans is " << lengthOfLIS(nums) << endl;
+    //printAllIncreasingSubsequences(nums);
 
     return 0;
 }
