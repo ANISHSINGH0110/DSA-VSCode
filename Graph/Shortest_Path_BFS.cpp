f42e7c637c74921e4cde445cb1d355c5d4afcdf8
ANISHSINGH0110 <anish2003s@gmail.com>
2025-03-03 10:14:38 +0530

    Shortest Path using BFS when weight are equal

---
+++ Graph/Shortest_Path_BFS.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<unordered_map>
+#include <vector>
+#include<list>
+#include<algorithm>
+#include<queue>
+using namespace std;
+class Graph{
+    public:
+      unordered_map<int,list<int>>adjList;
+      void addEdge(int u,int v,bool direction)
+      {
+        if(direction == 0)//undirected graph
+        {
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        else
+          adjList[u].push_back(v);
+      }
+      void shortestPathBFS(int src,int dest)
+      {
+         unordered_map<int,bool>vis;
+         unordered_map<int,int>parent;
+         queue<int>q;
+         q.push(src);
+         vis[src] = true;
+         parent[src] = -1;
+         while(!q.empty())
+         {
+            int frontNode = q.front();
+            q.pop();
+            for(auto nbr:adjList[frontNode])
+            {
+                if(!vis[nbr])
+                {
+                    q.push(nbr);
+                    vis[nbr] = true;
+                    parent[nbr] = frontNode;
+                }
+            }
+         }
+         vector<int>path;
+         int node = dest;
+         while(node != -1)
+         {
+            path.push_back(node);
+            node = parent[node];
+         }
+         reverse(path.begin(),path.end());
+         for(auto i:path)
+         {
+            cout << i << "->";
+         }
+         cout << endl;
+      }
+
+};
+int main()
+{
+
+    Graph g;
+    g.addEdge(0,1,0);
+    g.addEdge(1,2,0);
+    g.addEdge(2,3,0);
+    g.addEdge(3,5,0);
+    g.addEdge(3,4,0);
+    g.addEdge(4,6,0);
+    int src,dest;
+    cout << "Enter the source : "<< endl;
+    cin >> src;
+    cout << "Enter the dest : " << endl;
+    cin >> dest;
+
+    g.shortestPathBFS(src,dest);
+    return 0;
+}
