6867e524dd54e76c5f925f5c9efbeb70414a908e
ANISHSINGH0110 <anish2003s@gmail.com>
2025-04-17 11:28:42 +0530

    Balanced B.T

--- Tree/Binary_Tree/543. Diameter of Binary Tree.cpp
+++ Tree/Binary_Tree/543. Diameter of Binary Tree.cpp
@@ -12,25 +12,25 @@ class Node{
        }
 };
 int maxDepth(Node* root)
-    {
-        if(root == NULL)
-          return 0;
-        return 1+ max(maxDepth(root->left),maxDepth(root->right));  
-    }
-    int diameterOfBinaryTree(Node* root) {
-        if(root == NULL)
-          return 0;
-        int left = diameterOfBinaryTree(root->left);
-        int right = diameterOfBinaryTree(root->right);
-        int height = maxDepth(root->right)+maxDepth(root->left);  
-        return max(left,max(right,height));
-    }
-    int main(){
-        Node* root = new Node(3);
-        root->left = new Node(9);
-        root->right = new Node(21);
-        root->left->left = new Node(23);
-        root->left->right = new Node(39);
-        cout << "Maximum Diameter of B.t " << diameterOfBinaryTree(root) << endl;
-        return 0;
-    }
+{
+    if(root == NULL)
+     return 0;
+    return 1+ max(maxDepth(root->left),maxDepth(root->right));  
+}
+int diameterOfBinaryTree(Node* root) {
+    if(root == NULL)
+     return 0;
+    int left = diameterOfBinaryTree(root->left);
+    int right = diameterOfBinaryTree(root->right);
+    int height = maxDepth(root->right)+maxDepth(root->left);  
+    return max(left,max(right,height));
+}
+int main(){
+    Node* root = new Node(3);
+    root->left = new Node(9);
+    root->right = new Node(21);
+    root->left->left = new Node(23);
+    root->left->right = new Node(39);
+    cout << "Maximum Diameter of B.t " << diameterOfBinaryTree(root) << endl;
+    return 0;
+}
