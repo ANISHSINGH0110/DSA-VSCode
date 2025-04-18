e00352810b21766123376203128f1236ac683b97
ANISHSINGH0110 <anish2003s@gmail.com>
2025-04-18 15:39:38 +0530

    BST from Preorder Traversal

---
+++ Tree/BST/99. Recover Binary Search Tree.cpp
@@ -0,0 +1,19 @@
+TreeNode *first = NULL, *second = NULL, *prev = NULL;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+        if (prev && root->val < prev->val) {
+            if (!first) 
+              first = prev; 
+            second = root; 
+        }
+        
+        prev = root;
+        inorder(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val, second->val);
+    }
